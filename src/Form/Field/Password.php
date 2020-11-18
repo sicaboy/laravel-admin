85adf75817c8c047ba853681c9cfa8470cbcfc5f
David Shen <david@tonic.com.au>
2020-11-18 16:35:17 +1100

    avoid password being exposed when validation failed

--- src/Form/Field/Password.php
+++ src/Form/Field/Password.php
@@ -7,7 +7,8 @@ class Password extends Text
     public function render()
     {
         $this->prepend('<i class="fa fa-eye-slash fa-fw"></i>')
-            ->defaultAttribute('type', 'password');
+            ->defaultAttribute('type', 'password')
+            ->defaultAttribute('value', old($this->elementName ?: '', $this->value()));
 
         return parent::render();
     }
